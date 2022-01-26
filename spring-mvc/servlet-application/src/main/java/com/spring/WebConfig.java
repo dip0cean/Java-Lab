e640e74e2c9d12cda7df18657c184b2f8ab36054
Seongheon Sim <river.water.sea@gmail.com>
2022-01-26 23:40:23 +0900

    @EnableWebMvc + WebMvcConfigurer 사용해서 Bean 객체 커스터마이징 하기

--- spring-mvc/servlet-application/src/main/java/com/spring/WebConfig.java
+++ spring-mvc/servlet-application/src/main/java/com/spring/WebConfig.java
@@ -1,19 +1,20 @@
 package com.spring;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 // @ComponentScan 을 통해 모든 Bean 객체를 Spring IoC Container 에 등록한다.
 // 별도의 설정이 없다면 @Configuration 하위 패키지에 존재하는 컴포넌트 Bean 객체를 찾아 등록한다.
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
+/*
+    WebMvcConfigurer 인터페이스를 구현한다면 별도로 ViewResolver 를 구현하지 않아도 된다.
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,5 +22,11 @@ public class WebConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+ */
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
     }
 }
