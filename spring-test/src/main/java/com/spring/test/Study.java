553a443324f443e64f072abc3a063586f44de78d
Seongheon Sim <river.water.sea@gmail.com>
2022-03-07 23:44:37 +0900

    assertXXX 실습 코드

--- spring-test/src/main/java/com/spring/test/Study.java
+++ spring-test/src/main/java/com/spring/test/Study.java
@@ -1,4 +1,28 @@
 package com.spring.test;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.util.StringUtils;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Study {
+
+    private String  name;
+    private Status  status = Status.DRAFT;
+    private Integer limit;
+
+    public Study(Integer limit, String name) {
+        this.limit = limit;
+        this.name = name;
+        if (!StringUtils.hasText(name)) {
+            throw new NullPointerException("스터디명을 입력해주세요.");
+        }
+    }
+
+    public enum Status {
+        DRAFT, STARTED, ENDED
+    }
 }
---
+++ spring-test/src/test/java/com/spring/test/AssertTest.java
@@ -0,0 +1,48 @@
+package com.spring.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertTest {
+
+    @Test
+    @DisplayName("기본적인 Assert 테스트")
+    public void assertTest_1() {
+        Study study = new Study(-10, "Test Study");
+        // Assert 묶기 > assertAll 내부에 있는 assert 문 중에서 실패한 모든 assert 문을 체크할 수 있다.
+        assertAll(
+                // NotNull Check
+                () -> assertNotNull(study),
+                // Equals Check
+                () -> {
+                    study.setStatus(Study.Status.DRAFT);
+                    assertEquals(Study.Status.DRAFT, study.getStatus(), "study 의 상태를 \"Study.Status.DRAFT\" 를 기본값으로 설정해주세요.");
+                },
+                // True Check
+                () -> {
+                    study.setLimit(10);
+                    assertTrue(study.getLimit() > 1, "study 최대 참여 인원은 1명 이상이여야 합니다.");
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("AssertThrows 테스트")
+    public void assertTest_2() {
+        NullPointerException npe = assertThrows(NullPointerException.class, () -> new Study(10, ""));
+        assertEquals("스터디명을 입력해주세요.", npe.getMessage());
+    }
+
+    @Test
+    @DisplayName("AssertTimeout 테스트")
+    public void assertTest_3() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study();
+            Thread.sleep(50);
+        });
+    }
+}
